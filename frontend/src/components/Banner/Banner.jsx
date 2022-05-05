e948f88e5fb32304916882af7fe9fea2da32a3e3
dzungchaos <78160984+dzungchaos@users.noreply.github.com>
2022-05-05 08:13:52 +0700

    Add: Added Banner Interface

--- frontend/src/components/Banner/Banner.jsx
+++ frontend/src/components/Banner/Banner.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import styles from "./Banner.module.scss";
 
 function Banner() {
   return (
-    <div className="row">
-        Banner
+    <div className="rowBanner">
+      <div className="banner_container"> 
+        <img src="bannerimg.jpg" alt="SampleRoom" className="banner_illustration"></img>
+        <div className="banner_introduction">
+          <div className="banner_intro_title">FHomezRE</div>
+          <div className="banner_intro_description">
+            "Hệ thống tìm nhà trọ hiện đại được thành lập và vận hành bởi " <strong> FHomezRECorp </strong> ". "
+            <br/>
+            "Việc tìm kiếm một nhà trọ lý tưởng với bạn sẽ trở nên đơn giản hơn bao giờ hết. 👉👈 "
+            <br />
+            <button className="banner_intro_button_explore">
+              <Link to="/productItem"
+              className="banner_intro_button_explore_toItemList">
+              "Khám phá ngay 👈"
+              <i aria-hidden="true"></i>
+              </Link>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
