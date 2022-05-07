565efb9a53639d486041beada82c9169fd637847
Nam Ngo <henrynam28@gmail.com>
2022-05-07 10:57:38 +0700

    Test remote DB

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,30 +3,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import UploadForm from "./components/UploadForm/UploadForm";
+import ChatForm from "./components/ChatForm/ChatForm";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="signup" element={<Register />}>
-            <Route path="verifyOTP" element={<Register />}></Route>
-          </Route>
-          <Route path="login" element={<Login />}></Route>
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path="signup" element={<Register />}>
+                        <Route path="verifyOTP" element={<Register />}></Route>
+                    </Route>
+                    <Route path="login" element={<Login />}></Route>
 
-          {/* Path lồng nhau  */}
-          <Route path="teams" element={<Teams />}>
-            <Route path=":teamId" element={<Team />} />
-            <Route path="new" element={<NewTeamForm />} />
-            <Route index element={<LeagueStandings />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+                    {/* Path lồng nhau  */}
+                    <Route path="teams" element={<Teams />}>
+                        <Route path=":teamId" element={<Team />} />
+                        <Route path="new" element={<NewTeamForm />} />
+                        <Route index element={<LeagueStandings />} />
+                    </Route>
+                    <Route path="upload" element={<UploadForm />} />
+                </Routes>
+            </BrowserRouter>
 
-      {/* <ProductItem id="1" src="https://img.thuephongtro.com/images/thumb/2020/04/29/20200429120046-0zgfo.jpg" title="dfdfdfdfd" desc="hello" /> */}
-    </div>
-  );
+            {/* <ProductItem id="1" src="https://img.thuephongtro.com/images/thumb/2020/04/29/20200429120046-0zgfo.jpg" title="dfdfdfdfd" desc="hello" /> */}
+        </div>
+    );
 }
 
 export default App;
