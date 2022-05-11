314b74646ca9fc8363fee70c9a4d037871a30fcc
codeinlife10 <codeinlife10@gmail.com>
2022-05-11 10:15:37 +0700

    show productList on homepage

--- frontend/src/containers/ProductList.jsx
+++ frontend/src/containers/ProductList.jsx
@@ -1,18 +1,41 @@
 import React, { useEffect, useState } from "react";
+import { Card, Placeholder } from "react-bootstrap";
 import ProductItem from "../components/ProductItem/ProductItem";
+import { productService } from "../services/product.service";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    
-  });
+    async function getProductList() {
+      let result = await productService.getAll();
+      setIsLoading(false);
+      setProducts(result);
+    }
+
+    getProductList();
+  }, [products]);
+
 
   return (
-    <div className="col-8">
-      dffdf
-      {products &&
-        products.map((product) => 
+    <div className="col-9 d-grid gap-3">
+      {
+      isLoading ? 
+      <Card style={{ width: "100%", height: "100%"}}>
+        <Card.Body>
+          <Placeholder as={Card.Title} animation="glow">
+            <Placeholder xs={6} />
+          </Placeholder>
+          <Placeholder as={Card.Text} animation="glow">
+            <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{" "}
+            <Placeholder xs={6} /> <Placeholder xs={8} />
+          </Placeholder>
+          <Placeholder.Button variant="primary" xs={6} />
+        </Card.Body>
+      </Card> : 
+      products &&
+        products.map((product) => (
           <ProductItem
             key={product.id}
             id={product.id}
@@ -24,7 +47,7 @@ function ProductList() {
             location={product.location}
             createdAt={product.createdAt}
           ></ProductItem>
-        )}
+        ))}
     </div>
   );
 }
