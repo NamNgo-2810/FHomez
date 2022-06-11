ae736724de680d5d9d9131b02fb7f835d0990e4b
codeinlife10 <codeinlife10@gmail.com>
2022-06-11 12:37:54 +0700

    add axios config

--- frontend/src/apiConfig/axiosClient.js
+++ frontend/src/apiConfig/axiosClient.js
@@ -22,7 +22,7 @@ axiosClient.interceptors.request.use(function (config) {
 axiosClient.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    return response;
+    return response.data;
   }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
