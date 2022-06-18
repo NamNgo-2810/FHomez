e7a723e42cfdd3fe84f89f03cc508c132a3a5dd0
phongnt187270 <62378026+phongnt187270@users.noreply.github.com>
2022-06-18 11:24:52 +0700

    include auth to home API

--- backend/home_service/index.js
+++ backend/home_service/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const homeRoutes = require("./routes/homeRoutes");
-const reviewRoutes = require("./routes/reviewRoutes");
 const session = require("express-session");
 require("dotenv").config();
 
@@ -11,7 +10,6 @@ app.use(express.json());
 app.use(cors());
 app.use(session({ secret: "foo", resave: false, saveUninitialized: false }));
 app.use("/api/home", homeRoutes);
-app.use("/api/review", reviewRoutes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log("Home service listening on port " + PORT));
