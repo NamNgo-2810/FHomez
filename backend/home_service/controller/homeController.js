c0bc2c123f16811fe6e0aeeac874faf7fa3213a3
ngoductrung <trung10032000@gmail.com>
2022-06-18 10:01:51 +0700

    review -> home

--- backend/home_service/controller/homeController.js
+++ backend/home_service/controller/homeController.js
@@ -36,3 +36,42 @@ exports.search = async (req, res) => {
     // TO DO: Query all the records that contains information user entered,
     // and then sort by percentage of matching criterias
 };
+
+exports.getCommentByMotel = async (req, res) => {
+    try {
+        const result = await database.getCommentByMotel(motelid);
+        if (result) {
+            return res.status(200).json(result);
+        } else {
+            return res.status(404).send("This motel hasn't been reviewed yet");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.addReview = async (req, res) => {
+    try {
+        const result = await database.addReview();
+        if (result) {
+            return res.status(200).json(result);
+        } else {
+            return res.status(400).send("Success!");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+exports.deleteComment = async (req, res) => {
+    try {
+        const result = await database.deleteComment();
+        if (result) {
+            return res.status(200).json(result);
+        } else {
+            return res.status(400).send("Success!");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
--- backend/home_service/routes/homeRoutes.js
+++ backend/home_service/routes/homeRoutes.js
@@ -10,5 +10,8 @@ router.post("/rate_home", middleware.isAuth, controller.rateHome);
 router.get("/search", controller.search);
 router.get("/get_all_home", controller.getAllHome);
 router.get("/get_by_home_id", controller.getByHomeID);
+router.post("/review", middleware.isAuth, controller.addReview);
+router.get("/getCommentByMotel", controller.getCommentByMotel);
+router.delete("/deleteComment", middleware.isAuth, controller.deleteComment);
 
 module.exports = router;
