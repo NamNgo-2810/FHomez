6f5cd801f697ab1479442a07eb5a2f786f0f7d7a
ngoductrung <trung10032000@gmail.com>
2022-06-18 12:49:25 +0700

    admin routes

---
+++ backend/admin_service/admin.database.js
@@ -0,0 +1,46 @@
+const connection = require("./database");
+
+async function ownerRegisterApproval(user_id) {
+    return new Promise((resolve, reject) => {
+        connection.query(`UPDATE user SET role = 'owner' WHERE user_id = '${user_id}'`, (error, result) => {
+            if (error) reject(error);
+            resolve(result);
+        });
+    }).then((result) => {
+    
+        return result;
+    }).catch(error => console.log(error));
+}
+
+async function deleteUser(user_id) {
+    return new Promise((resolve, reject) => {
+        connection.query(`DELETE FROM user WHERE user_id = '${data.user_id}'`, (error, result) => {
+            if (error) reject(error);
+            resolve(result);
+        });
+    }).then((result) => {
+        if (result.length == 0) {
+            return null;
+        }
+        return result;
+    });
+}
+async function blogApproval(motel_id){
+    return new Promise((resolve, reject) => {
+        connection.query(`UPDATE motel SET status = 1 WHERE motel_id = '${motel_id}'`, (error, result) => {
+            if (error) reject(error);
+            resolve(result);
+        });
+    }).then((result) => {
+        if (result.length == 0) {
+            return null;
+        }
+        return result;
+    });
+}
+
+module.exports = {
+    deleteUser: deleteUser,
+    blogApproval: blogApproval,
+    ownerRegisterApproval: ownerRegisterApproval,
+};
