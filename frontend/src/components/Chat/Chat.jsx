bcc830a0ecf5d7c184b4d2d38af5512cfd1241d8
Nam Ngo <henrynam28@gmail.com>
2022-06-26 22:30:24 +0700

    Done realtime chat

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -1,8 +1,6 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
-import { useForm } from "react-hook-form";
 import axios from "axios";
 import { io } from "socket.io-client";
-import "./Chat.css";
 import AuthContext from "../../contexts/AuthContext";
 import {
     ChatContainer,
@@ -22,40 +20,49 @@ function Chat() {
     const [conversations, setConversations] = useState([]);
     const [currentChat, setCurrentChat] = useState(null);
     const [messages, setMessages] = useState([]);
-    const { register, handleSubmit, reset } = useForm({
-        defaultValues: {
-            message: undefined,
-        },
-    });
     const scrollRef = useRef();
     const socket = useRef(io("ws://localhost:8900"));
     const [arrivalMessage, setArrivalMessage] = useState(null);
+    const [message, setMessage] = useState("");
 
     const { user } = useContext(AuthContext);
-    console.log("user", user);
-    // const user = {
-    //     id: "123",
-    //     profilePicture:
-    //         "https://scontent-hkt1-1.xx.fbcdn.net/v/t39.30808-6/274923351_1337119300139137_3012035318960112960_n.jpg?_nc_cat=102&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=ds_b6ZVMWy8AX81iBPT&_nc_ht=scontent-hkt1-1.xx&oh=00_AT_Mrse_kwxS3R_rosAc0isg9YSkpLEbLx6jVBipcNaVXA&oe=627FE3AA",
-    //     username: "Nam",
-    // };
 
-    const onSend = async (e) => {
+    const getConversations = async () => {
+        try {
+            const res = await axios.get(
+                `http://localhost:5000/api/chat/conversation?userId=${user.user_id}`
+            );
+            setConversations(res.data);
+            setCurrentChat(res.data[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const getMessages = async () => {
+        try {
+            const res = await axios.get(
+                `http://localhost:5000/api/chat/message?conversationId=${currentChat._id}`
+            );
+            setMessages(res.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const onSend = async (text) => {
         const newMessage = {
-            sender: user.id,
-            text: e.message,
+            sender: user.user_id,
+            text: text,
             conversationId: currentChat._id,
         };
-        reset({
-            message: "",
-        });
 
         const receiverId = currentChat.members.find(
-            (member) => member !== user.id
+            (member) => member !== user.user_id.toString()
         );
 
         socket.current.emit("sendMessage", {
-            senderId: user.id,
+            senderId: user.user_id,
             receiverId: receiverId,
             text: newMessage.text,
         });
@@ -83,98 +90,117 @@ function Chat() {
     }, []);
 
     useEffect(() => {
-        arrivalMessage &&
-            currentChat?.members.includes(arrivalMessage.sender) &&
-            setMessages((prev) => [...prev, arrivalMessage]);
-    }, [arrivalMessage, currentChat]);
+        console.log("New message");
+        setMessages((prev) => [...prev, arrivalMessage]);
+    }, [arrivalMessage]);
 
     useEffect(() => {
-        socket.current.emit("addUser", user.id);
-        socket.current.on("getUsers", (users) => {
-            console.log(users);
-        });
+        socket.current.emit("addUser", user.user_id);
+        socket.current.on("getUsers", (users) => {});
     }, [user]);
 
     useEffect(() => {
-        scrollRef.current?.scrollIntoView({
-            behavior: "smooth",
-        });
-    }, [messages]);
-
-    useEffect(() => {
-        const getConversations = async () => {
-            try {
-                const res = await axios.get(
-                    `http://localhost:5000/api/chat/conversation?userId=${user.id}`
-                );
-                setConversations(res.data);
-                setCurrentChat(res.data[0]);
-                console.log("res", res);
-            } catch (error) {
-                console.log(error);
-            }
-        };
         getConversations();
-    }, [user.id]);
+    }, [user.user_id]);
 
     useEffect(() => {
-        const getMessages = async () => {
-            try {
-                const res = await axios.get(
-                    `http://localhost:5000/api/chat/message?conversationId=627a68fb2141eb8b9e643b9f`
-                );
-                setMessages(res.data);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        getMessages();
-    }, []);
+        if (currentChat) {
+            getMessages();
+        }
+    }, [currentChat]);
 
     return (
-        <div style={{ marginTop: "80px", height: "700px" }}>
+        <div style={{ marginTop: "80px", height: "400px" }}>
             <MainContainer>
                 <Sidebar position="left" scrollable={false}>
                     <ConversationList>
-                        <Conversation onClick={() => {}}>
-                            <Avatar
-                                status="available"
-                                src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000"
-                            />
-                            <Conversation.Content
-                                name="Nam"
-                                lastSenderName="Nam"
-                                info="Hello"
-                                style={{
-                                    display: "flex",
-                                    justifyContent: "flex-start",
-                                }}
-                            />
-                        </Conversation>
+                        {conversations?.map((conversation, index) => {
+                            return (
+                                <Conversation
+                                    onClick={() => {
+                                        setCurrentChat(conversation);
+                                    }}
+                                    key={`${index}`}
+                                >
+                                    <Avatar
+                                        name={
+                                            conversation.members[
+                                                conversation.members[0] ===
+                                                user.user_id.toString()
+                                                    ? 1
+                                                    : 0
+                                            ]
+                                        }
+                                        status="available"
+                                        src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000"
+                                    />
+                                    <Conversation.Content
+                                        name={
+                                            conversation.members[
+                                                conversation.members[0] ===
+                                                user.user_id.toString()
+                                                    ? 1
+                                                    : 0
+                                            ]
+                                        }
+                                        lastSenderName="Nam"
+                                    />
+                                </Conversation>
+                            );
+                        })}
                     </ConversationList>
                 </Sidebar>
 
                 <ChatContainer>
-                    <ConversationHeader>
-                        <ConversationHeader.Back />
-                        <Avatar src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000" />
-                        <ConversationHeader.Content
-                            userName="Nam"
-                            info="Active"
-                        />
-                    </ConversationHeader>
+                    {() => {
+                        if (currentChat) {
+                            return (
+                                <ConversationHeader>
+                                    <ConversationHeader.Back />
+                                    <Avatar src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000" />
+                                    <ConversationHeader.Content
+                                        userName={
+                                            currentChat?.members[0] ===
+                                            user.user_id.toString()
+                                                ? currentChat.members[0]
+                                                : currentChat.members[1]
+                                        }
+                                        info="Active"
+                                    />
+                                </ConversationHeader>
+                            );
+                        }
+                    }}
+
                     <MessageList>
-                        <Message
-                            model={{
-                                message: "Hello my friend",
-                                sentTime: "15 mins ago",
-                                sender: "Zoe",
-                                direction: "incoming",
-                                position: "single",
-                            }}
-                        />
+                        {messages?.map((message, index) => {
+                            return (
+                                <Message
+                                    model={{
+                                        message: message?.text,
+                                        sender: message?.sender,
+                                        direction:
+                                            message?.sender ===
+                                            user.user_id.toString()
+                                                ? "outgoing"
+                                                : "incoming",
+                                    }}
+                                    key={`${index}`}
+                                    avatarSpacer
+                                >
+                                    <Avatar src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000" />
+                                </Message>
+                            );
+                        })}
                     </MessageList>
-                    <MessageInput placeholder="Aa" onSend={onSend} />
+                    <MessageInput
+                        placeholder="Aa"
+                        onSend={onSend}
+                        value={message}
+                        onChange={(e) => {
+                            setMessage(e);
+                        }}
+                    />
                 </ChatContainer>
             </MainContainer>
         </div>
