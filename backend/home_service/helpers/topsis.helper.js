e3fc8899798c0aa8dd33ffb72d7c4279f1a36708
Nam Ngo <henrynam28@gmail.com>
2022-06-29 20:50:37 +0700

    done TOPSIS search

--- backend/home_service/helpers/topsis.helper.js
+++ backend/home_service/helpers/topsis.helper.js
@@ -1,8 +1,9 @@
 exports.sort = (location, data, weights) => {
+    // estimate the ideal
     const ideal = {
-        price: Math.max(...data.map((home) => home.price)),
+        price: Math.min(...data.map((home) => home.price)),
         area: Math.max(...data.map((home) => home.area)),
-        location: Math.min(
+        distance: Math.min(
             ...data.map((home) => {
                 return Math.sqrt(
                     Math.pow(home.latitude - location.latitude, 2) +
@@ -11,4 +12,36 @@ exports.sort = (location, data, weights) => {
             })
         ),
     };
+
+    // normalization
+
+    for (let item of data) {
+        const distance = Math.sqrt(
+            Math.pow(item.latitude - location.latitude, 2) +
+                Math.pow(item.longtitude - location.longtitude, 2)
+        );
+        item.normedPrice = (1 / (ideal.price * item.price)) * weights.price;
+        item.normedArea = (item.area / ideal.area) * weights.area;
+        item.normedLocation =
+            (1 / (ideal.distance * distance)) * weights.location;
+    }
+
+    const best = {
+        normedPrice: Math.max(...data.map((item) => item.normedPrice)),
+        normedArea: Math.max(...data.map((item) => item.normedArea)),
+        normedLocation: Math.max(...data.map((item) => item.normedLocation)),
+    };
+
+    // calculate the distances
+    for (let item of data) {
+        item.distanceToTheBest = Math.sqrt(
+            Math.pow(item.normedPrice - best.normedPrice, 2) +
+                Math.pow(item.normedArea - best.normedArea, 2) +
+                Math.pow(item.normedLocation - best.normedLocation, 2)
+        );
+    }
+
+    return data.sort((a, b) => {
+        return a.distanceToTheBest - b.distanceToTheBest;
+    });
 };
